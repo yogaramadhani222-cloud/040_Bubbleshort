bb1ed6a64fa9781277900536dfc9708a47260e64
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-05 16:11:39 +0700

    Add files via upload

---
+++ 040_Bubble_short.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input()
+{
+    int d;
+    while (true)
+    {
+    cout << "Masukkan banyaknya elemen pada array: ";
+    cin >> n;  
+    if (n <=20)
+        break;
+    else
+        {
+        cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan elemen array: " <<
+    cout << "======================" << endl;
+}
+for (int i = 0; i < n; i++)
+{
+    cout << "Data ke-" << (i + 1) << ": ";
+    cin >> arr[i];
+}
+
+void bubblesortarray()
+{
+    int pass = 1; // step 1
+
+    do
+    {
+        for (int j = 0; j < n - pass; j++)
+        { // step 2}
+            if (arr[j] > arr[j + 1])
+            { // step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass += 1; // step 4
+
+        cout << "\npass " << pass - 1 << ": "; // number of pass
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; // menampilkan data pada number of pass
+        }   
+        cout << endl;
+    
+    } while (pass <= n - 1); // step 5
+}
+
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Elemen array yang telah tersusun: " << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << " ";
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+
+    cout << "jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+}
+
+int main()
+{
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
