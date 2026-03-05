fe244ad05d0ad25713aecedf57354f73355cc4ce
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-05 16:16:51 +0700

    Add files via upload

---
+++ 040_bubblesort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input()
+{
+    int d;
+    while (true)
+    {
+    cout << "Masukkan banyaknya elemen pada array: ";
+    cin >> n;  
+    if (n <=20)
+        break;
+    else
+        {
+        cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
